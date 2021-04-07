8bc662a4c00762828ed6d83234951bacbc775d83
77Ilqar <77ilqar@gmail.com>
2021-04-07 10:17:50 +0400

    Basic UI

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,96 @@
+import { HttpParams } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { IProductBrand } from '../shared/models/productBrand';
+import { IProductType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', {static: true}) searchTerm: ElementRef;
+  products: IProduct[];
+  productBrands: IProductBrand[];
+  productTypes: IProductType[];
+  shopParams = new ShopParams();
+  totalCount: number;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ]
+
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getProductBrands();
+    this.getProductTypes();
+  }
+  getProducts()
+  {
+    this.shopService.getProducts(this.shopParams).subscribe(response => {
+      this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
+    }, error =>{ console.log('error');});
+  }
+  getProductBrands()
+  {
+    this.shopService.getProductBrands().subscribe(response => {
+      this.productBrands = [{id: 0, name: 'All'}, ...response];
+    }, error =>{ console.log('error');});
+  }
+  getProductTypes()
+  {
+    this.shopService.getProductTypes().subscribe(response => {
+      this.productTypes = [{id: 0, name: 'All'}, ...response];
+    }, error =>{ console.log('error');});
+  }
+  onBrandSelected(brandId :number)
+  {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+  onTypeSelected(typeId: number)
+  {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string)
+  {
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any)
+  {
+    if(this.shopParams.pageNumber !== event)
+    {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
+
